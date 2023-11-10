7bedbb0ec158e4206fe0ac39dcf833845f3ed0a3
Luka Londaridze <luka.londaridze@yellowjackets.bhsu.edu>
2023-11-10 17:52:48 +0400

    Initial commit.

---
+++ script.js
@@ -0,0 +1,33 @@
+let integer = 1;
+let float = 1.5;
+let bool = true;
+let string = 'Smart';
+
+let addition = integer + float;
+let subtraction = integer - float;
+let multiplication = integer * float;
+let division = integer / float;
+
+let Concatenation1 = string + 'Academy!';
+let Concatenation2 = `${string}Academy!`;
+
+let length = 7;
+let width = 5;
+let area = length * width;
+
+console.log('Integer:', integer);
+console.log('Float:', float);
+console.log('Boolean:', bool);
+console.log('String:', string);
+
+console.log('Addition :', addition);
+console.log('Subtraction :', subtraction);
+console.log('Multiplication :', multiplication);
+console.log('Division :', division);
+
+console.log('Concatenation with +:', Concatenation1);
+console.log('Concatenation with backticks:', Concatenation2);
+
+console.log('Length:', length);
+console.log('Width:', width);
+console.log('Area:', area);
